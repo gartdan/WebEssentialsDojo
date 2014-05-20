bb17b9ba4b9c7275b1180d72ab790a037819735f
Dan Gartner <dgartner@gmail.com>
2014-05-20 09:48:11 -0500

    Revert "Added less files"
    This reverts commit ca8024b88ff89c40c1fe3dd84f362efdbeb07fa4.

--- TypeScriptDemo/TypeScriptDemo/DemoStart/helpers.js
+++ TypeScriptDemo/TypeScriptDemo/DemoStart/helpers.js
@@ -1,48 +1,8 @@
-﻿/// <reference path="../scripts/typings/jquery/jquery.d.ts" />
-/// <reference path="../scripts/typings/jquery.color/jquery.color.d.ts" />
-var DemoModule;
-(function (DemoModule) {
-    var Product = (function () {
-        function Product(name, price) {
-            this.name = name;
-            this.price = price;
-        }
-        Product.prototype.getName = function () {
-            return this.name;
-        };
-        return Product;
-    })();
+﻿function sortByName(a) {
+    var result = a.slice(0);
+    result.sort(function (x, y) {
+        return x.name.localCompare(y.name);
+    });
+    return result;
+}
 
-    function sortByName(a) {
-        var result = a.slice(0);
-
-        result.sort(function (x, y) {
-            return x.getName().localeCompare(y.name);
-        });
-        return result;
-    }
-
-    var products = [
-        new Product("Bananas", 3.50),
-        new Product("Cabbage", 0.99),
-        new Product("Peaches", 2.00),
-        new Product("Artichoke", 2.00),
-        new Product("Gala Apples", 3.50),
-        new Product("Granny Smith Apples", 3.50)
-    ];
-
-    var sorted = sortByName(products);
-    alert(sorted[0].getName());
-    function animate() {
-        var block = $('#content');
-        var r = Math.floor(Math.random() * 256);
-        var b = Math.floor(Math.random() * 256);
-        var g = Math.floor(Math.random() * 256);
-        block.animate({ backgroundColor: $.Color(block.css('backgroundColor')).rgba([r, g, b, 1.0]) }, 500, animate);
-    }
-
-    $("#content").html(sorted[0].getName());
-    $("#content").fadeOut(500).delay(500).fadeIn(750).fadeOut(1000).fadeIn(1000).animate({ top: '-=400' });
-    animate();
-})(DemoModule || (DemoModule = {}));
-//# sourceMappingURL=helpers.js.map
